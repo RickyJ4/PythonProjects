891980bdbe1efc6403235071cf9472c25591b164
RickyJ <laurencemagenga12@gmail.com>
2024-03-25 16:38:38 -0700

    First OOP project

--- Budgets.py
+++ Budgets.py
@@ -1,43 +1,41 @@
+# Simple Budgets Project 
 import keyboard
 
-# Simple Budgets Project 
-
 #appending expenses into a list
-recreational_costs= []
+recreational = []
 fixed_costs = []
 Grocery = []
-amount = input('Enter amount you have : ')
+amount = int(input('Enter amount you have : '))
 
-# 
+#Adding expenses to the respective lists
 print('Calculating Expenses...')
 complete = False
 while not complete:
     print('Which costs do you want to add')
     print('1: Recreational costs \n2: Fixed costs \n3: Groceries')
     options = input('Enter selection : ')
-    print('Press D key when completed ')
+    print('Press 4 key when completed ')
     if(options == '1'):
         recreational_spending = int(input('Enter your recreational costs : '))
-        recreational_costs.append(recreational_costs)
+        recreational.append(recreational_spending)
     elif(options == '2'):
         fixed_spending = int(input('Enter your fixed costs : '))
         fixed_costs.append(fixed_spending)
-    else:
-        Groceries = input('Enter your Grocery costs : ')
+    elif(options == '3'):
+        Groceries = int(input('Enter your Grocery costs : '))
         Grocery.append(Groceries)
-
-    if keyboard.is_pressed('d'):
+    else: 
         complete = True
+#checking to see if list is being printed
+print(recreational)
+print(fixed_costs)
+print(Grocery)
 
-recreational_total = 0
-for i in range(recreational_costs):
-    recreational_total += i
-fixed_total = 0
-for i in range(fixed_costs):
-    fixed_total += i
-Grocery_total = 0
-for i in range(Grocery):
-    Grocery_total += i
+
+#adding up list totals 
+recreational_total = sum(recreational)
+fixed_total = sum(fixed_costs)
+Grocery_total = sum(Grocery)
 
 print(f'Money spent on Recreation = {recreational_total}, Groceries = {Grocery_total} and your fixed costs are {fixed_total}')
 amount_left = amount - (recreational_total + fixed_total + Grocery_total)
